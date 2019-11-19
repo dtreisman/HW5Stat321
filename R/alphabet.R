96471933d220e1bce7f2d91e513af089b56fbe87
Dani Treisman <danitreisman@gmail.com>
2019-11-19 11:26:35 -0600

    commit

---
+++ R/alphabet.R
@@ -0,0 +1,6 @@
+alphabet <-
+function() {
+  
+  letters <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+  sample(letters, 1, replace = F)
+}
